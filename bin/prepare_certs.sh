992f7f59928e14a79e2228233bb61aa91b82b3de
corpacademia <72709902+corpacademia@users.noreply.github.com>
2025-02-04 00:47:39 +0530

    Update prepare_certs.sh

--- bin/prepare_certs.sh
+++ bin/prepare_certs.sh
@@ -21,9 +21,9 @@ set -o pipefail         # pipefail exit after 1st piped commands failed
 export SCRIPT_NAME=$(basename "$0")
 export SCRIPT_BIN="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
 export SCRIPT_BASE="$(dirname ${SCRIPT_BIN})"
-export EMAIL=${EMAIL:-"admin@virtlab.site"}   # Adding a valid address is strongly recommended
-export HOSTNAME=${HOSTNAME:-"cloud"}
-export DOMAINNAME=${DOMAINNAME:-"virtlab.site"}
+export EMAIL="admin@virtlab.site"   # Adding a valid address is strongly recommended
+export HOSTNAME="cloud"
+export DOMAINNAME="virtlab.site"
 export STAGING_ENABLE=${STAGING_ENABLE:-0} # Set to 1 if you're testing your setup to avoid hitting request limits
 
 domains=(${HOSTNAME}.${DOMAINNAME})
